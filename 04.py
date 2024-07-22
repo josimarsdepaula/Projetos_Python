78a7a0945b35811af78af4f5450e12fe542ce550
Josimar <josimarsdepaula@gmail.com>
2024-07-22 12:18:48 -0300

    Exercicios02

---
+++ 04.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Insira a Primeira Nota bimestral: "))
+nota2 = float(input("Insira a Segunda Nota bimestral: "))
+nota3 = float(input("Insira a Terceira Nota bimestral: "))
+nota4 = float(input("Insira a Quarta Nota bimestral: "))
+
+soma = nota1 + nota2 + nota3 + nota4
+media = soma / 4
+
+print(f"A Média Final é:{media} ")
---
+++ 05.py
@@ -0,0 +1,3 @@
+valor = float(input("Digite um valor em Metros para conversão em Centímetros: "))
+converter = valor*100
+print(f" O valor: {valor} metros, convertido para centimetros é: {converter} centímetros")
